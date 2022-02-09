b6d94bc9f853b65a4a9ff844fb48bc4342e9aa95
Marco <vce10624@innovaccion.mx>
2022-02-09 17:17:47 -0600

    fppéndulodoble

--- GUI_TT_MIOPTION.py
+++ GUI_TT_MIOPTION.py
@@ -17,6 +17,20 @@ import matplotlib.animation as animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from numpy.linalg import inv
 
+import os
+from scipy import linalg
+from scipy.integrate import odeint 
+
+import time
+import pylab as py
+
+from matplotlib import animation, rc
+from IPython.display import HTML
+from matplotlib import pyplot as plt
+
+
+
+
 
 #---------------------Parametros DEPS-----------------------------#
 limit=[(0,10),(0,10),(0,10)]       # Limites inferior y superior
@@ -501,7 +515,211 @@ def inverted_pendulum(r,dimpi):
  
 #-----------------------------------------------------------------------------
 
+#-----------Péndulo doble-----------------------------------------------------
 
+#---------------------Parametros DE-----------------------------#
+limitpd=[[0,8],[0,5],[0,5],[0,5]]       # Limites inferior y superior
+poblacionpd = 200                    # Tamaño de la población, mayor >= 4
+f_mutpd = 0.5                        # Factor de mutacion [0,2]
+recombinationpd = 0.7                # Tasa de  recombinacion [0,1]
+generacionespd =  4               # Número de generaciones
+Dpd = 4                             # Dimensionalidad O número de variables de diseño 
+Mpd = 2                              # Numero de objetivos
+AMAXpd = 30                          # Numero maximo de soluciones en el archivo
+#----------------------------------------------------------------
+
+
+def double_pendulum(h,dinde):
+    #print(h)
+    '''Time parameters''' #Parametros temporales
+    dt = 0.005  # Tiempo de muestreo (5ms)
+    ti = 0.0  # Tiempo inicial de la simulación (0s)
+    tf =10  # Tiempo final de la simulación (12.25s)
+    n = int((tf - ti) / dt) + 1  # Número de muestras
+    t = np.linspace(ti, tf, n)  # Vector con los intsntes de tiempo (en Matlab 0:0.005:10)
+    
+    '''Dynamic parameters''' #Parametros dinamicos
+    m1 = dinde[0]  # Masa de la barra 1(kg)
+    m2= dinde[1] #Masa de la barra 2 (kg)
+    l1 = dinde[2] # Longitud de la barra 1 (m)
+    lc1 =dinde[3]  # Longitud al centro de masa de la barra 2 (m)
+    l2= dinde[4] #.0Longitud de la baraa 2 (m)
+    lc2=dinde[5] #Longitud al centro de masa de la barra 2(m)
+    b1 = dinde[6]  # Coeficiente de fricción viscosa de la barra 1
+    b2= dinde[7] #Coeficiente de fricción viscosa de la barra 2
+    gravi = 9.81  # Aceleración de la gravedad en la Tierra
+    I1 = dinde[8]  # Tensor de inercia del péndulo 1
+    I2= dinde[9]#Tensor de inercia del péndulo 2
+
+    ''' Cinematica inversa'''
+
+    r=0.2
+    #ro=r*np.cos(3*t)
+
+    '''Ecuaciones paramétricas de circunferencia'''
+    Xp =1.4+ r*np.cos(t)
+    Yp =0.2+ r*np.sin(t)
+    '''Ecuaciones paramétricas de rosa de 3 petalos
+    Xp =1.4+ ro*np.cos(t)
+    Yp =0.2+ ro*np.sin(t)   '''
+
+    #Ecuaciones pametricas lemniscata
+
+
+    '''%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Calculo del Modelo Cinematico Inverso de Posicion
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'''
+
+    #Variable Articular 2
+    cosq2=(Xp**2+Yp**2-l1**2-l2**2)/(2*l1*l2)
+    teta_rad_inv2=np.arctan2((1-cosq2**2)**(1/2),cosq2)
+
+    teta_rad_inv1= np.arctan2(Xp,-Yp)-np.arctan2(l2*np.sin(teta_rad_inv2),(l1+l2*np.cos(teta_rad_inv2)))
+
+    #teta_rad_inv2 =np.arccos((Xp**2+Yp**2-(l1**2+l2**2))/2*l1*l2)
+    teta_grad_inv2=teta_rad_inv2*180/np.pi
+
+
+    #Variable Articular 1 
+    '''alfa=np.arctan2(Xp,Yp)
+    beta=np.arccos((np.multiply(l1,l1)+np.multiply(l2,l2)-(np.multiply(Xp,Xp)+np.multiply(Yp,Yp)))/(2*l1*l2))
+    gamma=np.arcsin((l2*np.sin(beta))/np.sqrt(np.multiply(Xp,Xp)+np.multiply(Yp,Yp)))
+    '''
+
+    #teta_rad_inv1=np.arctan2(-Yp,Xp)-np.arctan2(l2*np.sin(teta_rad_inv2),l1+l2*np.cos(teta_rad_inv2))
+    teta_grad_inv1=teta_rad_inv1*180/np.pi
+    '''Cinematica Diferencial inversa'''
+    #Rosa de 3 petalos 
+    #dx=-r*(3*np.sin(3*t)*np.cos(t)+np.cos(3*t)*np.sin(t))
+    #dy=-r*(3*np.sin(3*t)*np.sin(t)-np.cos(3*t)*np.cos(t))
+    #Circunferencia 
+    dx=-r*np.sin(t)
+    dy=r*np.cos(t)
+
+    t1_dot=(((np.sin(teta_rad_inv1+teta_rad_inv2))/(l1*np.sin(teta_rad_inv2)))*dx)-((np.cos(teta_rad_inv1+teta_rad_inv2)/l1*np.sin(teta_rad_inv2))*dy)
+    t2_dot=-(((l1*np.sin(teta_rad_inv1)+l2*np.sin(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2))*dx)+(((l1*np.cos(teta_rad_inv1)+l2*np.cos(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2))*dy)
+    '''Cinematica Aceleración Inversa'''
+    #Aceleración circunferencia
+    ddx=-r*np.cos(t)
+    ddy=-r*np.sin(t)
+
+    #Aceleración rosa de 3 petalos
+    #ddx=-r*(10*np.cos(3*t)*np.cos(t)-6*np.sin(3*t)*np.sin(t))
+    #ddy=-r*(10*np.cos(3*t)*np.sin(t)+6*np.sin(3*t)*np.cos(t))
+
+    #Jacobiano inverso 
+    #Jinv= [[(np.sin(teta_rad_inv1+teta_rad_inv2)/l1*np.sin(teta_rad_inv2)), (-np.cos(teta_rad_inv1+teta_rad_inv2)/l1*np.sin(teta_rad_inv2))],[-(l1*np.sin(teta_rad_inv1)+l2*np.sin(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2),(l1*np.cos(teta_rad_inv1)+l2*np.cos(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2)]]
+    #Jt=np.array([[-r*np.cos(t)-(t1_dot**2*(-l1*np.sin(teta_rad_inv1)-l2*np.sin(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.sin(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.sin(teta_rad_inv1+teta_rad_inv2)))],[-r*np.sin(t)-(t1_dot**2*(l1*np.cos(teta_rad_inv1)-l2*np.cos(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.cos(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.cos(teta_rad_inv1+teta_rad_inv2)))]])
+
+    t1_ddot=((np.sin(teta_rad_inv1+teta_rad_inv2)/l1*np.sin(teta_rad_inv2))*(-r*np.cos(t)-(t1_dot**2*(-l1*np.sin(teta_rad_inv1)-l2*np.sin(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.sin(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.sin(teta_rad_inv1+teta_rad_inv2)))))+(((-np.cos(teta_rad_inv1+teta_rad_inv2)/l1*np.sin(teta_rad_inv2)))*(-r*np.sin(t)-(t1_dot**2*(l1*np.cos(teta_rad_inv1)-l2*np.cos(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.cos(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.cos(teta_rad_inv1+teta_rad_inv2)))))
+    t2_ddot=((-(l1*np.sin(teta_rad_inv1)+l2*np.sin(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2))*(-r*np.cos(t)-(t1_dot**2*(-l1*np.sin(teta_rad_inv1)-l2*np.sin(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.sin(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.sin(teta_rad_inv1+teta_rad_inv2)))))+(((l1*np.cos(teta_rad_inv1)+l2*np.cos(teta_rad_inv1+teta_rad_inv2))/l1*l2*np.sin(teta_rad_inv2))*(-r*np.sin(t)-(t1_dot**2*(l1*np.cos(teta_rad_inv1)-l2*np.cos(teta_rad_inv1+teta_rad_inv2))-2*t1_dot*t2_dot*l2*np.cos(teta_rad_inv1+teta_rad_inv2)-t2_dot**2*(l2*np.cos(teta_rad_inv1+teta_rad_inv2)))))
+
+    '''State variables'''#Variables de estado
+    x = np.zeros((n, 4))
+
+    '''Control vector'''#Señales de control
+    u = np.zeros(( 2,n))
+
+
+    '''Initial conditions'''#Condiciones iniciales
+    x[0, 0] =np.pi/2# Initial pendulum position 1 (rad)
+    x[0, 1] =0# Initial pendulum position 2 (rad)
+    x[0, 2]=0 # Initial pendulum velocity (rad/s)
+    x[0, 3]=0 # Initial pendulum velocity (rad/s)
+
+    th1_ddot=np.zeros(n)
+
+    ie_th1 = 0
+    ie_th2 = 0
+
+    '''State equation'''#Ecuacion de estado
+    xdot = [0, 0, 0, 0]
+
+    ise=0
+    iadu=0
+    ise_next=0
+    iadu_next=0
+
+    '''Dynamic simulation'''
+    for i in range(n - 1):
+        '''Current states'''
+        th1 = x[i, 0]
+        th2 = x[i, 1]
+        th1_dot=x[i,2]
+        th2_dot=x[i,3]
+    
+        '''Controller'''
+        M=np.array([[(m1*lc1**2)+I1+I2+m2*((l1**2)+(lc2**2)+(2*l1*lc2*np.cos(th2))),(m2*lc2**2)+I2+m2*l1*lc2*np.cos(th2)],[(m2*lc2**2)+I2+m2*l1*lc2*np.cos(th2), (m2*lc2**2)+I2]])
+        #Fuerzas centrípeta y de Coriolis
+        C=np.array([[-2*m2*l1*lc2*th2_dot*np.sin(th2) +b1 ,-m2*l1*lc2*np.sin(th2)*th2_dot],[m2*l1*lc2*th1_dot*np.sin(th2) , b2]])
+        #Aporte gravitacional
+        gra=np.array([[m1*lc1*gravi*np.sin(th1)+m2*gravi*(l1*np.sin(th1)+lc2*np.sin(th1+th2))],[m2*lc2*gravi*np.sin(th1+th2)]])
+    
+        e_th1 = teta_rad_inv1[i] - th1
+        e_th1_dot =t1_dot[i]- th1_dot
+    
+        e_th2 = teta_rad_inv2[i]- th2
+        e_th2_dot =t2_dot[i]- th2_dot
+
+        Kp =h[0]#5#10 #3.60614907877409#5.7255997347206#10
+        Kd =h[1]#10#0.973324679473922#5 #0.503359674635035#1.96901831751399#5
+        #Ki = 
+    
+        Kp2 =h[2]#4.93017386912806#5#3.60614907877409#5.7255997347206#5
+        Kd2 =h[3]#5#0.347734270091561#0.1#0.503359674635035#0.5554397672254#0.1
+        #Ki2 = 0
+
+        u[0,i] = Kp * e_th1 + Kd * e_th1_dot +M[0,0]*t1_ddot[i]+M[0,1]*t2_ddot[i]+C[0,0]*t1_dot[i]+C[0,1]*t2_dot[i]+gra[0,0]
+        u[1,i] = Kp2 * e_th2 + Kd2 * e_th2_dot +M[1,0]*t1_ddot[i]+M[1,1]*t2_ddot[i]+C[1,0]*t1_dot[i]+C[1,1]*t2_dot[i]+gra[1,0]
+    
+        '''Propiedades del modelo dinámico'''
+        #Efecto inercial
+   
+        v=np.array([[th1_dot],[th2_dot]])
+        C2=np.dot(C,v)
+        ua=np.array([[u[0,i]],[u[1,i]]])
+        aux1=ua-C2-gra
+        Minv=linalg.inv(M)
+        aux2=np.dot(Minv,aux1)
+        xdot[0] = th1_dot
+        xdot[1]= th2_dot
+        xdot[2]=aux2[0,:]
+        #th1_ddot[i]=xdot[3]
+        xdot[3]=aux2[1,:]
+        '''Integrate dynamics'''
+        x[i + 1, 0] = x[i, 0] + xdot[0] * dt
+        x[i + 1, 1] = x[i, 1] + xdot[1] * dt
+        x[i + 1, 2] = x[i, 2] + xdot[2] * dt
+        x[i + 1, 3] = x[i, 3] + xdot[3] * dt
+    
+        # ie_th1 = ie_th1 + e_th1 * dt
+        # ie_th2 = ie_th2 + e_th2 * dt
+    
+        ise=ise_next+(e_th1**2)*dt+(e_th2**2)*dt
+        iadu=iadu_next+ (abs(u[0,i]-u[0,i-1]))*dt+(abs(u[1,i]-u[1,i-1]))*dt
+        g=0
+        if(ise>=20):
+            ie=20
+            g+=1
+        else:
+                ie=ise
+                g+=0
+        if(iadu>=1):
+            ia=1
+            g+=1
+        else:
+            ia=iadu
+            g+=0
+        # if(g==2):
+        #     print(g)
+   
+        ise_next=ie
+        iadu_next=ia
+    # print(ise_next)
+    # print(iadu_next)
+    
+    return np.array([ise_next, iadu_next]),g,x,t,u
+#-----------------------------------------------------------------------------
 #Function for drawing
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -549,6 +767,23 @@ layouti=[[sg.Text('Péndulo Invertido:',text_color='white', font=('Franklin Goth
             [sg.Button(image_filename='D:\TT2\home.png', key='Homepi',image_subsample=8,button_color=(sg.theme_background_color(), sg.theme_background_color()))]
             ]
 
+layoutd=[[sg.Text('Péndulo Doble:',text_color='white', font=('Franklin Gothic Book', 28, 'bold')) ],
+            [sg.Text('Masa del brazo 1(m1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.Input('',key='masapd1')],
+            [sg.Text('Masa del brazo 2(m1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.Input('',key='masapd2')],
+            [sg.Text('Longitud del brazo 1 (l1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='lpd1')],
+            [sg.Text('Longitud al centro masa 1(lc1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='lcpd1')],
+            [sg.Text('Longitud del brazo 2 (l2):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='lpd2')],
+            [sg.Text('Longitud al centro masa 2(lc2):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='lcpd2')],
+            [sg.Text('Fricción del brazo 1(b1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText(key='bpd1')],
+            [sg.Text('Fricción del brazo 2(b2):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText(key='bpd2')],
+            [sg.Text('Momento de inercia  brazo 1(I1):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='ipd1')],
+            [sg.Text('Momento de inercia  brazo 2(I2):', text_color='black', font=('Franklin Gothic Book', 12, 'bold'),size=(24,1)), sg.InputText('',key='ipd2')],
+            [sg.Text('Seleccione el algoritmo metaheurístico:',text_color='white', font=('Franklin Gothic Book', 28, 'bold'))],
+            [sg.Button('DE',button_color='blue',border_width=3,key='depd'),sg.Button('GE',button_color='blue',border_width=3,key='gepd'),sg.Button('PSO',button_color='blue',border_width=3,key='psopd')],
+            [sg.Button(image_filename='D:\TT2\home.png', key='Homepd',image_subsample=8,button_color=(sg.theme_background_color(), sg.theme_background_color()))]
+            ]
+
+
 layoutpfps=[[sg.Text('Seleccione un conjunto de ganancias del controlador  PID',text_color='white', font=('Franklin Gothic Book', 20, 'bold'))],
             [sg.Table(values=[['Espera','estamos','ejecutando','código','......'],['Espera','estamos','ejecutando','código','......']] ,headings=['Kp' , 'Kd' ,' Ki','ISE','IADU'],auto_size_columns=True,right_click_selects=True,enable_click_events=True, key='Tabl',vertical_scroll_only=False,num_rows=25 ), sg.Canvas(key='can')],
             [sg.Button('Simular',key='Simups')]]
@@ -556,6 +791,10 @@ layoutpfps=[[sg.Text('Seleccione un conjunto de ganancias del controlador  PID',
 layoutpfpi=[[sg.Text('Seleccione un conjunto de ganancias del controlador  PID',text_color='white', font=('Franklin Gothic Book', 20, 'bold'))],
             [sg.Table(values=[['Espera','estamos','ejecutando','código','......','......','......','......'],['Espera','estamos','ejecutando','código','......','......','......','......']] ,headings=['Kpcar' , 'Kdcar' ,'Kicar','Kppénd' , 'Kdpén' ,' Kipén','ISE','IADU'],auto_size_columns=True,right_click_selects=True,enable_click_events=True, key='Tablpi',vertical_scroll_only=False,num_rows=25 ), sg.Canvas(key='canpfpi')],
             [sg.Button('Simular',key='Simupi')]]
+
+layoutpfpd=[[sg.Text('Seleccione un conjunto de ganancias del controlador  PID',text_color='white', font=('Franklin Gothic Book', 20, 'bold'))],
+            [sg.Table(values=[['Espera','estamos','ejecutando','código','......','......'],['Espera','estamos','ejecutando','código','......','......']] ,headings=['Kp1' , 'Kd1' ,'Kp2' , 'Kd2' ,'ISE','IADU'],auto_size_columns=True,right_click_selects=True,enable_click_events=True, key='Tablpd',vertical_scroll_only=False,num_rows=25 ), sg.Canvas(key='canpfpd')],
+            [sg.Button('Simular',key='Simupd')]]
 
 
 layoutsimpan=[[sg.Canvas(key='canani')]]
@@ -570,7 +809,13 @@ layoutinvpgra=[[sg.Canvas(key='cangrapi')]]
 layouttappi=[[sg.TabGroup([[sg.Tab('Animación',layoutinvpan),sg.Tab('Graficas', layoutinvpgra)]],tab_location='centertop',border_width=5)],
            [sg.Button(image_filename='D:\TT2\home.png', key='Homesimupi',image_subsample=8,button_color=(sg.theme_background_color(), sg.theme_background_color())),sg.Button('Salir',button_color='red',size=(3,2),border_width=5,key='Exit1')]]
 
-layout1=[[sg.Column(layouthome,key='Home'),sg.Column(layouts, visible=False,key='Sim'),sg.Column(layoutpfps,key='pfps',visible=False),sg.Column(layouttap,key='resps',visible=False),sg.Column(layouti,key='Inve',visible=False),sg.Column(layoutpfpi,key='pfpi',visible=False),sg.Column(layouttappi,key='respi',visible=False)]]
+layoutanpd=[[sg.Canvas(key='cananipd')]]
+layoutgrapd=[[sg.Canvas(key='cangrapd')]]
+
+layouttappd=[[sg.TabGroup([[sg.Tab('Animación',layoutanpd),sg.Tab('Graficas', layoutgrapd)]],tab_location='centertop',border_width=5)],
+           [sg.Button(image_filename='D:\TT2\home.png', key='Homesimupd',image_subsample=8,button_color=(sg.theme_background_color(), sg.theme_background_color())),sg.Button('Salir',button_color='red',size=(3,2),border_width=5,key='Exit2')]]
+
+layout1=[[sg.Column(layouthome,key='Home'),sg.Column(layouts, visible=False,key='Sim'),sg.Column(layoutpfps,key='pfps',visible=False),sg.Column(layouttap,key='resps',visible=False),sg.Column(layouti,key='Inve',visible=False),sg.Column(layoutpfpi,key='pfpi',visible=False),sg.Column(layouttappi,key='respi',visible=False),sg.Column(layoutd,key='Dob',visible=False),sg.Column(layoutpfpd,key='pfpd',visible=False),sg.Column(layouttappd,key='respd',visible=False)]]
         
 # #Create a fig for embedding.
 # fig = plt.figure(figsize=(5, 4))
@@ -590,10 +835,10 @@ while True:
     
     #u=float(p)
     #e=eval(values[0])
-    if event in (None, 'Exit','Exit0','Exit1'):
+    if event in (None, 'Exit','Exit0','Exit1','Exit2'):
         break
     if event == 'Simple':
-        #print(window['Simple'].get_size())
+        
         window['Home'].update(visible=False)
         window['Sim'].update(visible=True)
     if event == 'deps':
@@ -870,5 +1115,68 @@ while True:
     elif event=='Homesimupi':
         window['respi'].update(visible=False)
         window['Home'].update(visible=True)
+        
+    elif event == 'Doble':
     
+        window['Home'].update(visible=False)
+        window['Dob'].update(visible=True)
+        
+    elif event == 'depd':
+        window['Dob'].update(visible=False)
+        window['pfpd'].update(visible=True)
+        
+        m1=values['masapd1']
+        m2=values['masapd2']
+        l1=values['lpd1']
+        lc1=values['lcpd1']
+        l2=values['lpd2']
+        lc2=values['lcpd2']
+        b1=values['bpd1']
+        b2=values['bpd2']
+        isd1=values['ipd1']
+        isd2=values['ipd2']
+        
+        
+        dinpd=np.asarray([m1,m2,l1,lc1,l2,lc2,b1,b2,isd1,isd2], dtype=np.float64, order='C')
+        sg.popup('Ejecución de evolución diferencial, espera para poder observar el resultado (Conjunto de ganancias para el controlador PID).Las ganancias permitiran al péndulo llegar de la posición inial a la deseada .Presiona ok para continuar con la ejecución')
+        # dina=val_conver(dinps)
+        
+        #llamado de la función main de DE
+        varpd=main(double_pendulum, limitpd, poblacionpd, f_mutpd, recombinationpd, generacionespd,dinpd,Dpd,Mpd,AMAXpd)
+        
+        valupd=np.zeros((len(varpd[0]),(Dpd+Mpd)))
+
+        tpd=varpd[0]
+        spd=varpd[1]
+        
+        valupd[:,0]=spd[:,0]
+        valupd[:,1]=spd[:,1]
+        valupd[:,2]=spd[:,2]
+        valupd[:,3]=spd[:,3]
+        valupd[:,4]=tpd[:,0]
+        valupd[:,5]=tpd[:,1]
+        
+        #Create a fig for embedding.
+        figpd = plt.figure(figsize=(6, 5))
+        
+        ax30 = figpd.add_subplot(111)
+        ax30.set_title('Aproximación al frente de Pareto')
+        ax30.set_xlabel('f1')
+        ax30.set_ylabel('f2')
+        
+        #plot
+        ax30.scatter(tpd[:,0], tpd[:,1])
+        
+        fig_aggpd = draw_figure(window['canpfpd'].TKCanvas, figpd)
+        
+        window['Tablpd'].update(values=valupd)
+        #After making changes, fig_agg.draw()Reflect the change with.
+        fig_aggpd.draw()
+    
+    elif event=='Homepd':
+        window['Dob'].update(visible=False)
+        window['Home'].update(visible=True)
+    elif event=='Simupd':
+        window[''].update(visible=False)
+       
 window.close()
